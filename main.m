91ad1729c760c3295279480fe69aa5f82c1cb6cb
AndyLi <mlii0054@student.monash.edu>
2021-05-10 23:56:47 +0800

    Elaine todo

--- main.m
+++ main.m
@@ -2,10 +2,10 @@
 N=8;Q_factor=50;load_addr=".\data\data.mat";
 %load image
 load(load_addr);
-image=cell2mat(image);
-image1=double(image(:,:,1))-128;
-image2=double(image(:,:,2))-128;
-image3=double(image(:,:,3))-128;
+image=double(cell2mat(image));
+image1=(image(:,:,1))-128;
+image2=(image(:,:,2))-128;
+image3=(image(:,:,3))-128;
 test=[154 123 123 123 123 123 123 136;
 192	180 136 154 154 154 136 110;
 254 198 154 154 180 154 123 123;
@@ -14,9 +14,11 @@ test=[154 123 123 123 123 123 123 136;
 128 136 123 136 154 180 198 154;
 123 105 110 149 136 136 180 166;
 110 136 123 123 123 136 154 136]-128;
+%JPEG
 T=getDCTmatrix(N);
 D=Dct(image1,T);
 Q=getQ(N,2,Q_factor);
-compressed_image=compress(D,Q);
+compressed_image=compress(D,Q);%DCT domain
 depressed_image=depress(compressed_image,Q);
 retrived_image=iDCT(depressed_image,T);
+final_image=retrived_image+128;
